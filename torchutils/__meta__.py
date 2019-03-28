00b08d879b0f8798a7b91a6afb93d87023aeedc5
Scott Lowe <scott.code.lowe@gmail.com>
2019-03-28 17:50:02 -0300

    MAINT: Add __meta__.py

---
+++ torchutils/__meta__.py
@@ -0,0 +1,7 @@
+name = 'torchutils'
+path = 'torchutils'
+version = '0.1.0'
+author = 'Scott Lowe'
+author_email = 'scott.code.lowe@gmail.com'
+description = 'Utility functions for pytorch'
+url = 'https://github.com/scottclowe/pytorch'
