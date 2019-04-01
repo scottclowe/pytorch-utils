ed86fd41119e4f08c9f47f60c9dca56aff79904e
Scott Lowe <scott.code.lowe@gmail.com>
2019-04-01 10:47:56 -0300

    REL: Change version to 0.1.dev0 on master

--- torchutils/__meta__.py
+++ torchutils/__meta__.py
@@ -1,6 +1,6 @@
 name = 'torchutils'
 path = 'torchutils'
-version = '0.1.0'
+version = '0.1.dev0'
 author = 'Scott Lowe'
 author_email = 'scott.code.lowe@gmail.com'
 description = 'Utility functions for pytorch'
